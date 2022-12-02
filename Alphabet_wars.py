3f6c7985f8b249dccb20c16332373282bbfe8add
ferniprogrammer <fsanchor@myuax.com>
2022-12-02 15:21:42 +0100

    Variables definidas para Alphabetwars

---
+++ Alphabet_wars.py
@@ -0,0 +1,14 @@
+#Defino las letras que tienen un valor:
+#Equipo a:
+print("El equipo a son las letras w, p, b y s.")
+print("El equipo b son las letras m, q, d y z")
+print("El resto de letras son neutrales")
+w = 4
+p = 3
+b = 2
+s = 1
+m = 4
+q = 3
+d = 2
+z = 1
+
