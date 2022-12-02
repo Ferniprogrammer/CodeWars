e01baa7864037d1646400b2b3b95cf96761e3d59
ferniprogrammer <fsanchor@myuax.com>
2022-12-02 15:22:19 +0100

    Forma de contar las letras

--- Alphabet_wars.py
+++ Alphabet_wars.py
@@ -11,4 +11,14 @@ m = 4
 q = 3
 d = 2
 z = 1
+#Intro
+text = input("Introduzca letras cualquiera")  
+cant_w = text.count("w")
+cant_p = text.count("p")
+cant_b = text.count("b")
+cant_s = text.count("s")
+cant_m = text.count("m")
+cant_q = text.count("q")
+cant_d = text.count("d")
+cant_z = text.count("z")
 
